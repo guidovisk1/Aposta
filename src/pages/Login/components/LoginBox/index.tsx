d3403fd2aa369105af77e951b3ce56974e26dbd8
IuriPires <iuripires.work@gmail.com>
2022-04-07 19:04:16 -0300

    Login page almost finished

--- src/pages/Login/components/LoginBox/index.tsx
+++ src/pages/Login/components/LoginBox/index.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
+import { useAuth } from '../../../../hooks/Auth/auth';
+
+import Button from '../../../../components/Button';
+import Checkbox from '../../../../components/Checkbox';
+import Input from '../../../../components/Input';
 
 import {
   Container,
@@ -8,21 +14,60 @@ import {
   ButtonsWrapper,
   ForgetPasswordText,
 } from './styles';
-import Input from '../../../../components/Input';
-import Button from '../../../../components/Button';
-import Checkbox from '../../../../components/Checkbox';
 
 const LoginBox: React.FC = () => {
+  const auth = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignIn = async () => {
+    setIsLoading(true);
+    await auth
+      .signIn({ email, password })
+      .catch(() => {
+        Swal.fire({
+          title: 'Algo deu errado!',
+          text: 'Algo deu errado durante o processo de login. Revise as informações e tente novamente.',
+          icon: 'error',
+          confirmButtonColor: '#FF5427',
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const handleOnChange = (e: { target: { name: string; value: any } }) => {
+    const { name, value } = e.target;
+
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+
   return (
     <Container>
       <MainTitle>Sistema de Manutenção e Inspenção</MainTitle>
       <SecondaryText>Acesse sua conta</SecondaryText>
 
-      <Input labelText="Login" placeholder="Login de usuário" />
-      <Input labelText="Senha" placeholder="*****" type="password" />
+      <Input
+        labelText="Login"
+        placeholder="Login de usuário"
+        onChange={handleOnChange}
+        name="email"
+      />
+      <Input
+        labelText="Senha"
+        placeholder="*****"
+        type="password"
+        onChange={handleOnChange}
+        name="password"
+      />
       <ButtonsWrapper>
         <Checkbox labelText="Lembrar usuário" />
-        <Button>Login</Button>
+        <Button disabled loading={isLoading} onClick={() => handleSignIn()}>
+          Login
+        </Button>
       </ButtonsWrapper>
 
       <LineSeparator />
