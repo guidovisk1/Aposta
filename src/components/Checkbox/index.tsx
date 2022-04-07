5cc52e14f5e4d2b30689572b3df11b5c73fd7181
IuriPires <iuripires.work@gmail.com>
2022-04-07 19:45:57 -0300

    Added some helpers to identify elements for testing purposes

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -4,13 +4,20 @@ import { Container, HTMLCheckbox, LabelText } from './styles';
 
 interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   labelText?: string;
+  disabled?: boolean;
 }
 
 const Checkbox: React.FC<CheckboxProps> = ({ labelText, ...rest }) => {
   return (
-    <Container className="container">
-      <HTMLCheckbox className="checkmark" {...rest} type="checkbox" />
-      <LabelText>{labelText}</LabelText>
+    <Container data-testid="checkbox-wrapper" className="container">
+      <HTMLCheckbox
+        id="checkbox"
+        data-testid="checkbox-input"
+        className="checkmark"
+        {...rest}
+        type="checkbox"
+      />
+      <LabelText data-testid="checkbox-label">{labelText}</LabelText>
     </Container>
   );
 };
