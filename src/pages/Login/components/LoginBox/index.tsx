b0d07007ca50fbd3b36aee30df0748ed79cec001
IuriPires <iuripires.work@gmail.com>
2022-04-09 13:27:49 -0300

    Added validation to Login form

--- src/pages/Login/components/LoginBox/index.tsx
+++ src/pages/Login/components/LoginBox/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
+import { useFormik } from 'formik';
 import { useAuth } from '../../../../hooks/Auth/auth';
 
 import Button from '../../../../components/Button';
@@ -15,34 +16,56 @@ import {
   ForgetPasswordText,
 } from './styles';
 
+import validations from './validations';
+
 const LoginBox: React.FC = () => {
   const auth = useAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSignIn = async () => {
-    setIsLoading(true);
-    await auth
-      .signIn({ email, password })
-      .catch(() => {
-        Swal.fire({
-          title: 'Algo deu errado!',
-          text: 'Algo deu errado durante o processo de login. Revise as informações e tente novamente.',
-          icon: 'error',
-          confirmButtonColor: '#FF5427',
-        });
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+  const showErrorModal = () => {
+    return Swal.fire({
+      title: 'Algo deu errado!',
+      text: 'Algo deu errado durante o processo de login. Revise as informações e tente novamente.',
+      icon: 'error',
+      confirmButtonColor: '#FF5427',
+    });
   };
 
-  const handleOnChange = (e: { target: { name: string; value: any } }) => {
-    const { name, value } = e.target;
+  const {
+    dirty,
+    isValid,
+    values,
+    errors,
+    touched,
+    handleBlur,
+    setFieldValue,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: validations,
+    onSubmit: async () => {
+      setIsLoading(true);
+      await auth
+        .signIn({ email: values.email, password: values.password })
+        .catch(() => {
+          showErrorModal();
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    },
+  });
 
-    if (name === 'email') setEmail(value);
-    if (name === 'password') setPassword(value);
+  const handleEmailChangeText = (value: string) => {
+    setFieldValue('email', value);
+  };
+
+  const handlePasswordChangeText = (value: string) => {
+    setFieldValue('password', value);
   };
 
   return (
@@ -51,23 +74,40 @@ const LoginBox: React.FC = () => {
       <SecondaryText>Acesse sua conta</SecondaryText>
 
       <Input
+        hasError={
+          touched.email && errors.email ? Boolean(errors.email) : undefined
+        }
+        value={values.email}
+        errorMessage={errors.email ? errors.email : ''}
         labelText="Login"
-        hasError
-        errorMessage="E-mail inválido..."
+        onBlur={handleBlur}
         placeholder="Login de usuário"
-        onChange={handleOnChange}
+        onChange={e => handleEmailChangeText(e.target.value)}
         name="email"
       />
       <Input
+        hasError={
+          touched.password && errors.password
+            ? Boolean(errors.password)
+            : undefined
+        }
+        value={values.password}
+        errorMessage={errors.password ? errors.password : ''}
+        onChange={e => handlePasswordChangeText(e.target.value)}
+        onBlur={handleBlur}
         labelText="Senha"
         placeholder="*****"
         type="password"
-        onChange={handleOnChange}
         name="password"
       />
       <ButtonsWrapper>
         <Checkbox labelText="Lembrar usuário" />
-        <Button loading={isLoading} onClick={() => handleSignIn()}>
+        <Button
+          disabled={!dirty || !isValid}
+          loading={isLoading}
+          onClick={() => handleSubmit()}
+          type="button"
+        >
           Login
         </Button>
       </ButtonsWrapper>
