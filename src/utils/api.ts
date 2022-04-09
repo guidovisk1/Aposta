22f62cefce6fbe55942b7ab35ffdc1dda2336f4f
IuriPires <iuripires.work@gmail.com>
2022-04-09 14:06:24 -0300

    Setting token in header

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'https://api-easysupport.azurewebsites.net/api/',
+  headers: {
+    token: localStorage.getItem('@GOIT:token') as string,
+  },
 });
