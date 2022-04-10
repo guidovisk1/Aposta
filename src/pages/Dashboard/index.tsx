1b15a361f6dedcc9383e7c9ade98850216163c4a
IuriPires <iuripires.work@gmail.com>
2022-04-10 16:14:35 -0300

    Starting dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import menuOptions from '../../utils/menu';
 
 import { Container } from './styles';
 
+import SideMenu from './components/SideMenu';
+
 const Dashboard: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpening = (parameter: boolean) => {
+    setIsOpen(parameter);
+  };
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <SideMenu
+        handleOpening={handleOpening}
+        isOpen={isOpen}
+        menuOptions={menuOptions}
+      />
     </Container>
   );
 };
