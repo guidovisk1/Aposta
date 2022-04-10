50955c095ea407f8a40b6a2e2471fee00b1a5f64
IuriPires <iuripires.work@gmail.com>
2022-04-10 12:47:54 -0300

    Preparing for menu component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,12 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Login} />
+    <Route path="/dashboard" isPrivate component={Dashboard} />
   </Switch>
 );
 
