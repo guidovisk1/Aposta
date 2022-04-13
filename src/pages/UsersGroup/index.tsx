9c13960d263cff24ffe50d1f1cf67193c64f4c38
IuriPires <iuripires.work@gmail.com>
2022-04-13 16:44:44 -0300

    Placed the right route to UsersGroup page and started the boilerplate

---
+++ src/pages/UsersGroup/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import SideInfoPanel from '../../components/SideInfoPanel';
+
+const UsersGroup: React.FC = () => {
+  return (
+    <Container className="page-container">
+      <SideInfoPanel title="Grupos" />
+    </Container>
+  );
+};
+
+export default UsersGroup;
