f3c0107803467223735ff66addf86cb9dbf46e36
IuriPires <iuripires.work@gmail.com>
2022-04-16 17:39:05 -0300

    Side panel now receives some new props

--- src/components/SideInfoPanel/index.tsx
+++ src/components/SideInfoPanel/index.tsx
@@ -7,18 +7,25 @@ import addIcon from '../../assets/icons/add-icon.svg';
 
 interface SideInfoPanelProps {
   title: string;
+  openForm?: () => void;
 }
 
-const SideInfoPanel: React.FC<SideInfoPanelProps> = ({ title }) => {
+const SideInfoPanel: React.FC<SideInfoPanelProps> = ({
+  title,
+  openForm,
+  children,
+}) => {
   return (
     <Container>
       <HeaderWrapper>
         <HeaderTitle>{title}</HeaderTitle>
         <Input width="180px" height="45px" placeholder="Pesquisar..." />
       </HeaderWrapper>
-      <AddButton>
+      <AddButton onClick={openForm}>
         <img src={addIcon} alt="Adicionar" />
       </AddButton>
+
+      {children}
     </Container>
   );
 };
