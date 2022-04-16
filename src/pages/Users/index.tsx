9d91f0bf9ad69f3847d8c7b4bc45302caa587b54
IuriPires <iuripires.work@gmail.com>
2022-04-16 17:39:19 -0300

    Trying to retrieve users from API

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container } from './styles';
 
 import SideInfoPanel from '../../components/SideInfoPanel';
+import ListItems from './components/ListItem';
+import Form from './components/Form';
+
+import { getUsers } from './user.service';
 
 const Users: React.FC = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getAllUsers = async () => {
+      const { data } = await getUsers();
+      setUsers(data);
+    };
+    getAllUsers();
+  }, []);
+
+  function clickMe() {
+    console.log('Fui clicado e apareci no USERS page');
+  }
+
+  function openForm() {
+    console.log('Abri o modal');
+  }
+
   return (
     <Container className="page-container">
-      <SideInfoPanel title="Usuários" />
+      <SideInfoPanel openForm={() => openForm()} title="Usuários">
+        {users.length > 0 && (
+          <ListItems list={users} onClicked={() => clickMe()} />
+        )}
+      </SideInfoPanel>
+
+      <Form title="Adicionar um Usuário" />
     </Container>
   );
 };
