be76be617bcecdfc9d18e03f27e3a7be12cd228c
IuriPires <iuripires.work@gmail.com>
2022-04-20 16:45:15 -0300

    Adding new update method into user.service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,15 +1,17 @@
+/* eslint-disable camelcase */
 import { api, endpoints } from '../utils';
 
 interface User {
-  codUsuario: string;
-  userGroup: string;
-  userName: string;
-  userEmail: string;
-  password: string;
-  userRole: string;
-  userHourRate: number;
-  userSector: string;
-  userRegistrationCode: string;
+  cod_grupousuarios: string;
+  cod_usuario: string;
+  custo_hora: number;
+  email: string;
+  funcao: string;
+  grupoUsuarios: string;
+  matricula: string;
+  nome: string;
+  senha: string;
+  setor: string;
 }
 
 export const getUsers = () => {
@@ -17,25 +19,50 @@ export const getUsers = () => {
 };
 
 export const createUser = ({
-  codUsuario,
-  userGroup,
-  userName,
-  userEmail,
-  password,
-  userRole,
-  userHourRate,
-  userSector,
-  userRegistrationCode,
-}: User) => {
+  cod_grupousuarios,
+  cod_usuario,
+  custo_hora,
+  email,
+  funcao,
+  matricula,
+  nome,
+  senha,
+  setor,
+}: // TODO userStatus - waiting API implementation
+User) => {
   return api.post(endpoints.USERS.GET_ALL, {
-    cod_usuario: codUsuario,
-    cod_grupousuarios: userGroup,
-    nome: userName,
-    email: userEmail,
-    senha: password,
-    funcao: userRole,
-    custo_hora: userHourRate,
-    setor: userSector,
-    matricula: userRegistrationCode,
+    cod_usuario,
+    cod_grupousuarios,
+    nome,
+    email,
+    senha,
+    funcao,
+    custo_hora,
+    setor,
+    matricula,
+  });
+};
+
+export const updateUser = (
+  id: string,
+  {
+    cod_usuario,
+    cod_grupousuarios,
+    nome,
+    email,
+    funcao,
+    custo_hora,
+    setor,
+  }: // TODO userStatus - waiting API implementation
+  Partial<User>,
+) => {
+  return api.put(`${endpoints.USERS.GET_ALL}/${id}`, {
+    cod_usuario,
+    cod_grupousuarios,
+    nome,
+    email,
+    funcao,
+    custo_hora,
+    setor,
   });
 };
