759391c9a26a5165427b3a87b96fc1dede1e4b36
IuriPires <iuripires.work@gmail.com>
2022-04-20 16:45:02 -0300

    Few changes on form validation

--- src/pages/Users/components/Form/validations.ts
+++ src/pages/Users/components/Form/validations.ts
@@ -1,17 +1,14 @@
 import * as yup from 'yup';
 
 export default yup.object().shape({
-  login: yup.string().required('O campo Login é obrigatório'),
-  userEmail: yup
+  email: yup
     .string()
     .required('O endereço de E-mail é obrigatório')
     .email('Formato de email inválido'),
-  password: yup.string().required('O campo Senha é obrigatório'),
-  userRole: yup.string().required('O campo Função é obrigatório'),
-  userGroup: yup.string().required('O campo Grupo é obrigatório'),
-  userName: yup.string().required('O campo Nome é obrigatório'),
-  userRegistrationCode: yup
-    .string()
-    .required('O campo Matrícula é obrigatório'),
-  userStatus: yup.boolean().required('O campo Status é obrigatório'),
+  senha: yup.string().required('O campo Senha é obrigatório'),
+  funcao: yup.string().required('O campo Função é obrigatório'),
+  cod_grupousuarios: yup.string().required('O campo Grupo é obrigatório'),
+  nome: yup.string().required('O campo Nome é obrigatório'),
+  matricula: yup.string().required('O campo Matrícula é obrigatório'),
+  // userStatus: yup.string().required('O campo Status é obrigatório'),
 });
