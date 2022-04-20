19da1b0146c69b7dd96cc96eabd9f7b064adb623
IuriPires <iuripires.work@gmail.com>
2022-04-20 16:45:38 -0300

    Changing header name to accomplish backend's name

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://api-easysupport.azurewebsites.net/api/',
   headers: {
-    token: `Bearer ${localStorage.getItem('@GOIT:token') as string}`,
+    Authorization: `Bearer ${localStorage.getItem('@GOIT:token') as string}`,
   },
 });
