1fc30c9a94c978b65134db940f442b66b6c53e78
IuriPires <iuripires.work@gmail.com>
2022-04-27 17:04:42 -0300

    style and index file of EPI's module

---
+++ src/pages/EPIs/index.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+
+import { Container } from './styles';
+
+import SideInfoPanel from '../../components/SideInfoPanel';
+
+import { getEpis } from '../../services/epis.service';
+
+import ListItem from './components/ListItem';
+import Form from './components/Form';
+
+interface EPI {
+  cod_epi: string;
+  descricao: string;
+  imagem?: string;
+  status?: number;
+}
+
+const EPIs: React.FC = () => {
+  const [epis, setEpis] = useState<EPI[]>([] as EPI[]);
+
+  const [searchedEpi, setSearchedEpi] = useState<EPI[]>([] as EPI[]);
+
+  const [selectedEpi, setSelectedEpi] = useState<EPI>({} as EPI);
+
+  useEffect(() => {
+    async function fetchEpis() {
+      const { data } = await getEpis();
+      const episMapped = data.map((epi: EPI) => {
+        return { ...epi, status: epi.status ? 1 : 0 };
+      });
+      setEpis(episMapped);
+    }
+
+    fetchEpis();
+  }, []);
+
+  function handleEpiSelection(selected: EPI) {
+    setSelectedEpi(selected);
+  }
+
+  function openForm() {
+    setSelectedEpi({} as EPI);
+  }
+  function onSearch(searchedValue: string) {
+    if (searchedValue === undefined || searchedValue === '')
+      setSearchedEpi([] as EPI[]);
+    setSearchedEpi(
+      epis.filter(epi =>
+        epi.descricao.toLowerCase().includes(searchedValue.toLocaleLowerCase()),
+      ),
+    );
+  }
+  return (
+    <Container className="page-container">
+      <SideInfoPanel
+        title="EPIs"
+        onSearch={searched => onSearch(searched)}
+        openForm={() => openForm()}
+      >
+        <ListItem
+          list={searchedEpi.length ? searchedEpi : epis}
+          onClicked={epi => handleEpiSelection(epi)}
+        />
+      </SideInfoPanel>
+
+      <Form title="Adicionar um EPI" epiSelected={selectedEpi} />
+    </Container>
+  );
+};
+
+export default EPIs;
