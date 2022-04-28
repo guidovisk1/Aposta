be5a78a465b67e098ee92829f276d9018dc7d0e2
IuriPires <iuripires.work@gmail.com>
2022-04-28 20:23:58 -0300

    Training main structure

---
+++ src/pages/Trainings/index.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+
+import { Container } from './styles';
+
+import SideInfoPanel from '../../components/SideInfoPanel';
+
+import { getTrainings } from '../../services/trainings.service';
+
+import ListItem from './components/ListItem';
+import Form from './components/Form';
+
+interface Training {
+  cod_treinamento: string;
+  descricao: string;
+  status?: number;
+}
+
+const EPIs: React.FC = () => {
+  const [trainings, setTrainings] = useState<Training[]>([] as Training[]);
+
+  const [searchedTraining, setSearchedTraining] = useState<Training[]>(
+    [] as Training[],
+  );
+
+  const [selectedTraining, setSelectedTraining] = useState<Training>(
+    {} as Training,
+  );
+
+  useEffect(() => {
+    async function fetchTrainings() {
+      const { data } = await getTrainings();
+      const trainingsMapped = data.map((training: Training) => {
+        return { ...training, status: training.status ? 1 : 0 };
+      });
+      setTrainings(trainingsMapped);
+    }
+
+    fetchTrainings();
+  }, []);
+
+  function handleTrainingSelection(selected: Training) {
+    setSelectedTraining(selected);
+  }
+
+  function openForm() {
+    setSelectedTraining({} as Training);
+  }
+  function onSearch(searchedValue: string) {
+    if (searchedValue === undefined || searchedValue === '')
+      setSearchedTraining([] as Training[]);
+    setSearchedTraining(
+      trainings.filter(training =>
+        training.descricao
+          .toLowerCase()
+          .includes(searchedValue.toLocaleLowerCase()),
+      ),
+    );
+  }
+  return (
+    <Container className="page-container">
+      <SideInfoPanel
+        title="EPIs"
+        onSearch={searched => onSearch(searched)}
+        openForm={() => openForm()}
+      >
+        <ListItem
+          list={searchedTraining.length ? searchedTraining : trainings}
+          onClicked={training => handleTrainingSelection(training)}
+        />
+      </SideInfoPanel>
+
+      <Form
+        title="Adicionar um Treinamento"
+        trainingSelected={selectedTraining}
+      />
+    </Container>
+  );
+};
+
+export default EPIs;
