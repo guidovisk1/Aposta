fc0255a54f84a91668d7e47a9f842c66b277fba7
IuriPires <iuripires.work@gmail.com>
2022-04-28 21:10:44 -0300

    Finished User Groups main structure

--- src/pages/UsersGroup/index.tsx
+++ src/pages/UsersGroup/index.tsx
@@ -1,15 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container } from './styles';
 
 import SideInfoPanel from '../../components/SideInfoPanel';
 
-const UsersGroup: React.FC = () => {
+import { getUserGroups } from '../../services/userGroup.service';
+
+import ListItem from './components/ListItem';
+import Form from './components/Form';
+
+interface IUserGroup {
+  cod_grupoUsuarios: string;
+  descricao: string;
+  status?: number;
+}
+
+const UserGroup: React.FC = () => {
+  const [userGroups, setuserGroup] = useState<IUserGroup[]>([] as IUserGroup[]);
+
+  const [searchedUserGroup, setSearchedTraining] = useState<IUserGroup[]>(
+    [] as IUserGroup[],
+  );
+
+  const [selectedTraining, setSelectedUserGroup] = useState<IUserGroup>(
+    {} as IUserGroup,
+  );
+
+  useEffect(() => {
+    async function fetchuserGroup() {
+      const { data } = await getUserGroups();
+      const userGroupMapped = data.map((userGroup: IUserGroup) => {
+        return { ...userGroup, status: userGroup.status ? 1 : 0 };
+      });
+      setuserGroup(userGroupMapped);
+    }
+
+    fetchuserGroup();
+  }, []);
+
+  function handleUserGroupelection(selected: IUserGroup) {
+    setSelectedUserGroup(selected);
+  }
+
+  function openForm() {
+    setSelectedUserGroup({} as IUserGroup);
+  }
+  function onSearch(searchedValue: string) {
+    if (searchedValue === undefined || searchedValue === '')
+      setSearchedTraining([] as IUserGroup[]);
+    setSearchedTraining(
+      userGroups.filter(userGroup =>
+        userGroup.descricao
+          .toLowerCase()
+          .includes(searchedValue.toLocaleLowerCase()),
+      ),
+    );
+  }
   return (
     <Container className="page-container">
-      <SideInfoPanel onSearch={() => console.log('Oi')} title="Grupos" />
+      <SideInfoPanel
+        title="Grupo.."
+        onSearch={searched => onSearch(searched)}
+        openForm={() => openForm()}
+      >
+        <ListItem
+          list={searchedUserGroup.length ? searchedUserGroup : userGroups}
+          onClicked={userGroup => handleUserGroupelection(userGroup)}
+        />
+      </SideInfoPanel>
+
+      <Form
+        title="Adicionar um Treinamento"
+        userGroupSelected={selectedTraining}
+      />
     </Container>
   );
 };
 
-export default UsersGroup;
+export default UserGroup;
