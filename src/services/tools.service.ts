295115188109e8e68fb350c0956acb0ccb44a3be
IuriPires <iuripires.work@gmail.com>
2022-05-09 16:23:16 -0300

    changing the way information is persisted into backend

--- src/services/tools.service.ts
+++ src/services/tools.service.ts
@@ -9,30 +9,10 @@ export const getTools = () => {
   return api.get(endpoints.TOOL.GET_ALL);
 };
 
-export const createTool = ({
-  codFerramenta,
-  descricao,
-  imagem,
-  status,
-}: Tool) => {
-  const data = { codFerramenta, descricao, imagem, status };
-  return api.post(endpoints.TOOL.GET_ALL, data, {
-    responseType: 'arraybuffer',
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+export const createTool = (formData: Tool) => {
+  return api.post(endpoints.TOOL.GET_ALL, formData);
 };
 
-export const updateTool = ({
-  codFerramenta,
-  descricao,
-  imagem,
-  status,
-}: Tool) => {
-  return api.put(`${endpoints.TOOL.GET_ALL}/${codFerramenta}`, {
-    descricao,
-    imagem,
-    status,
-  });
+export const updateTool = (codFerramenta: string, formData: Tool) => {
+  return api.put(`${endpoints.TOOL.GET_ALL}/${codFerramenta}`, formData);
 };
